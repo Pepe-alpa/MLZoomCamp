4ba6893a4ccb91a292fe811ea6d197fefbd6ba2e
Pepe-Alpa <josealpa.1098@gmail.com>
2023-11-27 12:59:36 -0600

    Tarea 09 serverless

---
+++ 09-serverless/lambda_function.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import numpy as np
+import os
+#from keras.optimizers import SGD
+
+#import tensorflow.lite as tflite
+import tflite_runtime.interpreter as tflite
+from io import BytesIO
+from urllib import request
+
+from PIL import Image
+
+MODEL_NAME = os.getenv('MODEL_NAME', 'bees-wasps-v2.tflite')
+
+def download_image(url):
+    with request.urlopen(url) as resp:
+        buffer = resp.read()
+    stream = BytesIO(buffer)
+    img = Image.open(stream)
+    return img
+
+
+def prepare_image(img, target_size):
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img = img.resize(target_size, Image.NEAREST)
+    return img
+
+def prepare_input(x):
+    return x / 255.0
+
+interpreter = tflite.Interpreter(model_path=MODEL_NAME)
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+# https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg
+
+def predict(url):
+    image = download_image(url)
+    image = prepare_image(image, target_size=(150, 150))
+    
+
+    x = np.array(image, dtype='float32')
+    X = np.array([x])
+    X = prepare_input(X)
+
+
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+
+    return float(preds[0,0])
+
+def lambda_handler(event, context):
+    url = event['url']
+    pred = predict(url)
+    result = {
+        'prediction': pred
+    }
+
+    return result
+
